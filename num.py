750fbcf22b4bd4872880e49f300dd3ef03a3f53d
Ayaan-Singh <asthana.divya@gmail.com>
2024-12-02 18:37:13 +0530

    hi

---
+++ num.py
@@ -0,0 +1,11 @@
+import random
+a = (random.randint(10,15))
+while True:
+ b = int(input("enter a num between 10 to 15 "))
+ if (a == b):
+   print("match")
+   break
+ else:
+   print("not matching")
+ 
+
